3a50301f8069849231d5f21e86f61aeceaa8c734
Shivani <shivani.gupta@talentica.com>
2025-02-11 12:59:00 +0530

    added two new files

---
+++ NumberProgramming/Addition.js
@@ -0,0 +1,13 @@
+/*Simple addition
+let x= 10
+let y= 20
+console.log("addition of two number:", x+y)
+*/
+
+
+//using function
+let x,y
+function add(x,y){
+    console.log("addition of two numbers:",x+y)
+}
+add(10,20)
---
+++ NumberProgramming/Prime.js
@@ -0,0 +1,30 @@
+/*using if else
+let x=29
+if(x%2==0){
+    console.log(x+ "Number is prime number")
+}
+else 
+console.log(x+"number is non prime number")
+*/
+
+/*Using function
+function isPrime(x){
+    if(x%2==0){
+        console.log("prime")
+    }else
+    console.log("Non Prime")
+        
+}
+isPrime(23)
+*/
+
+//using for loop
+
+let n=19
+let isPrime = true
+if(n<2){
+    isPrime= false
+}
+for(i=2;i<=n;i++){
+    if(n%i==0)
+}
